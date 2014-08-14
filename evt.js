b2582cf7b865920a826331955e1b0b6565ba1613
hotoo <hotoo.cn@gmail.com>
2014-08-14 22:15:40 +0800

    Fixed more event handler support on remove

--- evt.js
+++ evt.js
@@ -23,6 +23,7 @@ function Event (){
         for (var i = list.length - 1; i >= 0; i--) {
           if (list[i] === handler) {
             list.splice(i, 1);
+            break;
           }
         }
       }
