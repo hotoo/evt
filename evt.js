2d0c1a07197d65edd6481bba73af5b21590156fa
hotoo <hotoo.cn@gmail.com>
2014-08-17 13:20:49 +0800

    Refact for remove all listeners and return

--- evt.js
+++ evt.js
@@ -17,29 +17,29 @@ Event.prototype = {
     var me = this;
     var listeners = me._;
 
-    // Remove *all* events
     if (listeners){
 
+      // Remove *all* events
       if (!(eventName || handler)) {
         me._ = {};
-        return me;
-      }
+      } else {
 
-      var list = listeners[eventName];
-      if (list) {
-        if (handler) {
-          for (var i = list.length - 1; i >= 0; i--) {
-            if (list[i] === handler) {
-              list.splice(i, 1);
-              break;
+        var list = listeners[eventName];
+        if (list) {
+          if (handler) {
+            for (var i = list.length - 1; i >= 0; i--) {
+              if (list[i] === handler) {
+                list.splice(i, 1);
+                break;
+              }
             }
           }
+          else {
+            delete listeners[eventName];
+          }
         }
-        else {
-          delete listeners[eventName];
-        }
-      }
 
+      }
     }
 
     return me;
